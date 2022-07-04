f2059e19fd03b710ff71c7a36f4ad3d5866dbbab
Ahmed Ebeid <ebeidahmed2@gmail.com>
2022-07-03 20:42:40 -0700

    Unused code had deleted

--- app/src/main/java/com/example/weatherapp/staticTexts.java
+++ app/src/main/java/com/example/weatherapp/staticTexts.java
@@ -1,9 +1,8 @@
 package com.example.weatherapp;
 
 public class staticTexts {
-    private static final  String API_KEY="aa8d28f6cbb3bca2c8a27760d5651dcf";
+    private static final  String API_KEY=BuildConfig.API_KEY;
     public static final String USER_LOCATION_DB_CREATION="CREATE TABLE userLocation(id INTEGER PRIMARY KEY,lon VARCHAR(30) NOT NULL, lat VARCHAR(30) NOT NULL,city VARCHAR(15) NOT NULL);";
-    public static final String CITY_DB_CREATION="CREATE TABLE cities(city VARCHAR(20) NOT NULL);";
     public static final String REMOVE_IF_EXISTS_USER_LOCATION_DB="DROP TABLE IF EXISTS userLocation";
     public static final String FROM_FORECAST_REPORT_ACTIVITY="FORECAST";
     public static final String FROM_SEARCH_ACTIVITY="SEARCH ";
